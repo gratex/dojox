a77a78af1dcc601efaf934eda6a566e8553acd71
Andrej Kumor <akumor@gratex.com>
2012-08-28 17:29:12 +0200

    [FIX] allow delayedHeaderFocus on EnhancedGrid in IE9

--- grid/enhanced/_FocusManager.js
+++ grid/enhanced/_FocusManager.js
@@ -474,7 +474,7 @@ return declare("dojox.grid.enhanced._FocusManager", _FocusManager, {
 	_delayedHeaderFocus: function(){
 		// summary:
 		//		Overwritten
-		if(this.isNavHeader() && has('ie')>8){
+		if(this.isNavHeader() && !(has('ie')<9)){
 			this.focusHeader();
 		}
 	},
